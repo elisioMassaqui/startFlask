b454f0376f169ae7f8d602346c8128867ade46a6
Mr.Robot <elisiomassaqui8@gmail.com>
2024-08-03 13:23:12 +0100

    tudo cabe numa lista txe

--- app.py
+++ app.py
@@ -5,15 +5,18 @@ web = Flask(__name__)
 @web.route("/")
 def HELLO():
     print('OI')
+    y = 'variavel y'
+    z = 4505
     TypeError = 5 #Erro de tipo recebido pelo navegador
     #A lista ele retornou pra navegador
-    listaAceitavel = [1, 2, 3, 4, 5]
+    listaAceitavel = [1, 2, 3, 4, 5, 'eliso', True, False, not True, 3.78, y, z]
     #Mas o objecto não!
     x3 = {'nome': 'elisio', 'idade': 22}
-    pegarTodasChaves = print(x3.keys())
+    printTodasChaves = print(x3.keys())
+    pegarTodasChaves = x3.keys()
     #AttributeError return  x3, listaAceitavel, pegarTodasChaves
     #talvez return f"""x3:{x3}, listaAceitavel:{listaAceitavel}, pegarTodasChaves{pegarTodasChaves}"""
-    return  listaAceitavel, pegarTodasChaves, x3
+    return  listaAceitavel, x3
 
 
 if __name__ == "__main__":
